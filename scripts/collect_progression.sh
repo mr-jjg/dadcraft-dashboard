e91f44758df7c928db3eea2da73fe834954f22b3
Joel J Gerard <git@jjg.dev>
2026-05-07 16:25:30 -0700

    feat: add guild filter to population progression

--- scripts/collect_progression.sh
+++ scripts/collect_progression.sh
@@ -14,15 +14,19 @@ VALUES ($SCRAPED_AT);
 
 SET @scrape_id = LAST_INSERT_ID();
 
-INSERT INTO dadcraft_dashboard.progression_snapshots (scrape_id, level, race, class, online, count)
+INSERT INTO dadcraft_dashboard.progression_snapshots (scrape_id, level, race, class, online, guild, count)
 SELECT @scrape_id, c.level,
        CASE c.race WHEN 1 THEN 'Human' WHEN 2 THEN 'Orc' WHEN 3 THEN 'Dwarf' WHEN 4 THEN 'Night Elf' WHEN 5 THEN 'Undead' WHEN 6 THEN 'Tauren' WHEN 7 THEN 'Gnome' WHEN 8 THEN 'Troll' ELSE 'Unknown' END,
        CASE c.class WHEN 1 THEN 'Warrior' WHEN 2 THEN 'Paladin' WHEN 3 THEN 'Hunter' WHEN 4 THEN 'Rogue' WHEN 5 THEN 'Priest' WHEN 7 THEN 'Shaman' WHEN 8 THEN 'Mage' WHEN 9 THEN 'Warlock' WHEN 11 THEN 'Druid' ELSE 'Unknown' END,
-       c.online, COUNT(*)
+       c.online,
+       COALESCE(g.name, 'None') AS guild,
+       COUNT(*)
 FROM v_characters.characters c
 JOIN v_realmd.account a ON c.account = a.id
+LEFT JOIN v_characters.guild_member gm ON c.guid = gm.guid
+LEFT JOIN v_characters.guild g ON gm.guildid = g.guildid
 WHERE a.gmlevel = 0
-GROUP BY c.level, c.race, c.class, c.online;
+GROUP BY c.level, c.race, c.class, c.online, g.name;
 
 INSERT INTO dadcraft_dashboard.progression_snapshot_chars (scrape_id, guid)
 SELECT @scrape_id, c.guid
