38476fa2afc667f206b511a851214664571bce0b
Joel J Gerard <git@jjg.dev>
2026-06-18 22:59:56 -0700

    feat: add eight zone skins (Blackrock Depths, Hinterlands, Maraudon, Razorfen Downs, Scarlet Monastery, Shadowfang Keep, Stranglethorn Vale, Wailing Caverns)

--- frontend/src/constants/themes.js
+++ frontend/src/constants/themes.js
@@ -1,4 +1,11 @@
 export const THEMES = [
     { value: 'azshara',            label: 'Azshara' },
+    { value: 'blackrock_depths',            label: 'Blackrock Depths' },
+    { value: 'hinterlands',            label: 'Hinterlands' },
+    { value: 'maraudon',            label: 'Maraudon' },
+    { value: 'razorfen_downs',    label: 'Razorfen Downs'},
+    { value: 'scarlet_monastery',    label: 'Scarlet Monastery'},
+    { value: 'shadowfang_keep',    label: 'Shadowfang Keep'},
     { value: 'stranglethorn_vale', label: 'Stranglethorn Vale' },
+    { value: 'wailing_caverns', label: 'Wailing Caverns' },
 ]
